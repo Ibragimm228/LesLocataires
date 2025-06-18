a79e72382db768ab9521e7fea162df3e802b66f1
Ibragimm228 <arimaxgn@gmail.com>
2025-06-18 18:09:13 +0500

    first commitgssg

--- src/services/comments.service.ts
+++ src/services/comments.service.ts
@@ -22,7 +22,7 @@ class CommentsService {
 		const comments = [...(article.comments || []), newComment]
 
 		const { error } = await supabase
-			.from('justkit_articles')
+			.from('racolta_articles')
 			.update({ comments })
 			.eq('id', articleId)
 			.select()
